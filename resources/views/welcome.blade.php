b07d021201f74c630868f2f03da786ab9a2c10fd
Patrick Rodriguez <ianpatrick.rodriguez@benilde.edu.ph>
2019-05-18 10:09:04 +0800

    test update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+    <textarea class="form-control" id="summary-ckeditor" name="summary-ckeditor"></textarea>
+
+    <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace( 'summary-ckeditor' );
+        CKEDITOR.config.extraPlugins = 'justify';
+        CKEDITOR.config.extraPlugins = 'easyimage';
+        CKEDITOR.config.extraPlugins = 'image2';
+        CKEDITOR.config.extraPlugins = 'page2images';
+    </script>
+    </body>
+</html>
